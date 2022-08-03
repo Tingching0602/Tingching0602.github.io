0e6d21c0b08383a5be4642378b8e75293465b609
Anya Chen <53967230+Tingching0602@users.noreply.github.com>
2022-08-03 20:23:19 +0800

    test

--- glintshop/vue.config.js
+++ glintshop/vue.config.js
@@ -1,3 +1,9 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/glintshop/" : "/",
+
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/glintshop/dist/" : "/",
-}
+  // publicPath 為 project-name，根目錄地址為上傳的網域
+  publicPath: '/glintshop/',
+};
