8cb54c8f12c9140780ab4ab81766a34065925b4d
Anya Chen <53967230+Tingching0602@users.noreply.github.com>
2022-08-03 20:37:49 +0800

    remove glintshop folder

--- glintshop/src/views/NotFound.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>找不到404</h1>
-</template>
--- glintshop/vue.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/glintshop/" : "/",
-
-// }
-
-module.exports = {
-  // publicPath 為 project-name，根目錄地址為上傳的網域
-  publicPath: '/glintshop/',
-};
